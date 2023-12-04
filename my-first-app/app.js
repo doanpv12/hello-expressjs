7184e2549b8a3ce18116a2471cc0c69a4d7ad3ca
luci1812 <lucinguyen1812@gmail.com>
2023-12-04 14:46:37 +0700

    Hello sample

---
+++ my-first-app/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+  res.send('<div style="color:red;">Hello from express</div>')
+})
+
+app.listen(3000,()=>{
+  console.log(`This is log from my sample run at port: 3000`);
+})
