8e0f8361525ccec5798d8fe39666d5c2c52c4298
luci1812 <lucinguyen1812@gmail.com>
2023-12-04 16:15:50 +0700

    add !

--- my-first-app/app.js
+++ my-first-app/app.js
@@ -8,5 +8,5 @@ app.get('/',(req,res)=>{
 
 app.listen(3000,()=>{
   console.log(`This is log from my sample run at port: 3000`);
-  console.log('One more log!!')
+  console.log('One more log!!!!')
 })
